e3fd97cf182b415e43961d556a2f6f336c2f1dc7
Saul Nuñez <saul.nunez99@gmail.com>
2022-05-13 15:23:07 -0600

    (WIP) Rendering

---
+++ Titan/Ed/Markup/GeminiMarkup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Titan.Ed
+{
+    public static class GeminiMarkup
+    {
+
+    }
+}
